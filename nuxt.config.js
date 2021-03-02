5c79b2c585063c9c004e27a64312c4adf32ca6f7
bizoton19 <bizoton19@gmail.coim>
2021-03-02 08:51:04 -0500

    initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,63 @@
+export default {
+  env: {
+    strapiBaseUri: process.env.API_URL || "http://localhost:1337"
+  },
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'dontbebrokebewoke',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Staatliches'},
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    'uikit/dist/css/uikit.min.css',
+    'uikit/dist/css/uikit.css',
+    
+    '@assets/css/main.css'
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    { src: '~/plugins/uikit.js', ssr: false },
+    
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    '@nuxtjs/apollo',
+    
+  ],
+  
+  apollo: {  
+    clientConfigs: {
+      default: {
+        httpEndpoint: process.env.BACKEND_URL || "http://localhost:1337/graphql"
+      }
+    }
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">
+        By Adrien Salomon
+      </h1>
+      <p class=".uk-text-lead">Business | Investor | Finance geek | Podcaster</p>
+      <div class="links">
+        <a
+          href="https://www.instagram.com/dontbebrokebewoke/"
+          uk-icon="icon: instagram"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Instagram
+        </a>
+        <a
+          href="https://nuxtjs.org/"
+          uk-icon="icon: twitter"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Twitter
+        </a>
+        <a
+          href="https://nuxtjs.org/"
+          uk-icon="icon: facebook"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Facebook
+        </a>
+        <a
+          href="/blog"
+          uk-icon="icon: pencil"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Blog
+        </a>
+        <a
+          href="https://podcasts.apple.com/us/podcast/dontbebrokebewoke/id1532142721"
+          uk-icon="icon: microphone"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Podcast
+        </a>
+        
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
